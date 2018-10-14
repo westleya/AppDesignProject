e485f31e0d0ef8b22be9de42d3a044aadf272c19
JunchenZ <jack_zhangjunchen@163.com>
2018-10-14 17:48:24 -0600

    weather part

--- LifestyleApp/app/src/main/java/com/example/lifestyleapp/WeatherViewModel.java
+++ LifestyleApp/app/src/main/java/com/example/lifestyleapp/WeatherViewModel.java
@@ -1,14 +1,26 @@
 package com.example.lifestyleapp;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
-public class WeatherViewModel extends ViewModel {
-    private final MutableLiveData<WeatherData> jsonData = new
-            MutableLiveData<WeatherData>();
+public class WeatherViewModel extends AndroidViewModel {
+    private MutableLiveData<WeatherData> jsonData;
+    private WeatherRepository mWeatherRepository;
 
-    public LiveData<WeatherData> getData() {
+    public WeatherViewModel(Application application){
+        super(application);
+        mWeatherRepository = new WeatherRepository(application);
+        jsonData = mWeatherRepository.getData();
+    }
+    public void setLocation(String location){
+        mWeatherRepository.setLocation(location);
+    }
+
+    public LiveData<WeatherData> getData(){
         return jsonData;
     }
+
 }
